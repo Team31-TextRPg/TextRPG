e0dd9fb5b25d908f252fea1858c956e76a8b98a1
amond2 <8hyojae@gmail.com>
2025-02-07 20:37:47 +0900

    merge : 전달용 파일

--- TextRPG/TextRPG/Program.cs
+++ TextRPG/TextRPG/Program.cs
@@ -1,10 +1,10 @@
-﻿// namespace TextRPG
-// {
-//     internal class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             Console.WriteLine("Hello, World!");
-//         }
-//     }
-// }
+﻿namespace TextRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
