589b6bf261f42348b90fea6eef1abbd4fed2bdc0
JinY0321 <sunplant0321@gmail.com>
2025-02-07 12:38:12 +0900

    feat : JY-Player 생성자 매개변수 수정

--- TextRPG/TextRPG/Player.cs
+++ TextRPG/TextRPG/Player.cs
@@ -1,25 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
-public class Player
+namespace TextRPG
 {
-    public int level;
-    public string name;
-    public string jobClass;
-    public float attack;
-    public float depense;
-    public int health;
-    public int gold;
-
-    // 생성자
-    public Player(string name)
+    internal class Player
     {
-        Name = name;
-        Level = 1;
-        MaxHealth = 100;
-        Health = MaxHealth;
-        AttackPower = 10;
-        Defense = 5;
+        public string character { get; private set; }
+        public string jobClass { get; private set; }
+        public int level { get; private set; }
+        public int attack { get; private set; }
+        public int defense { get; private set; }
+        public int health { get; private set; }
+        public int maxHealth { get; private set; }
+        public int gold { get; private set; }
+
+        public Player(string name, string job, int level, int attackPower, int defensePower, int health, int gold) // 플레이어 생성자
+        {
+            character = name;
+            jobClass = job;
+            level = level;
+            attack = attackPower;
+            defense = defensePower;
+            maxHealth = health;
+            gold = gold;
+        }
+
+        public void ShowStatus()
+        {
+            Console.WriteLine($"Lv. {level}");
+            Console.WriteLine($"{character} ({jobClass})");
+            Console.WriteLine($"공격력: {attack}");
+            Console.WriteLine($"방어력: {defense}");
+            Console.WriteLine($"체 력: {health} / {maxHealth}");
+            Console.WriteLine($"Gold: {gold} G");
+        }
     }
-
-
 }
