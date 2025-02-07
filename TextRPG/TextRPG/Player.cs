6db2717a8b6c9957a688b45fda8a5564bdeced73
JinY0321 <sunplant0321@gmail.com>
2025-02-07 12:28:44 +0900

    feat : Player 클래스 프로퍼티 및 생성자 수정

--- TextRPG/TextRPG/Player.cs
+++ TextRPG/TextRPG/Player.cs
@@ -8,6 +8,7 @@ using System.Xml.Linq;
 
 namespace TextRPG
 {
+<<<<<<< Updated upstream
     internal class Player
     {
         public int level;
@@ -39,4 +40,36 @@ namespace TextRPG
         }
     }
     }
+=======
+    public string character { get; private set; }
+    public string jobClass { get; private set; }
+    public int level { get; private set; }
+    public int attack { get; private set; }
+    public int defense { get; private set; }
+    public int health { get; private set; }
+    public int maxHealth { get; private set; }
+    public int gold { get; private set; }
+
+    public Player(string name, string job, int level, int attackPower, int defensePower, int health, int gold) // 플레이어 생성자
+    {
+        character = name;
+        jobClass = job;
+        level = level;
+        attack = attackPower;
+        defense = defensePower;
+        maxHealth = health;
+        gold = gold;
+    }
+
+    public void ShowStatus()
+    {
+        Console.WriteLine($"Lv. {level}");
+        Console.WriteLine($"{character} ({jobClass})");
+        Console.WriteLine($"공격력: {attack}");
+        Console.WriteLine($"방어력: {defense}");
+        Console.WriteLine($"체 력: {health} / {maxHealth}");
+        Console.WriteLine($"Gold: {gold} G");
+    }
+     
+>>>>>>> Stashed changes
 }
