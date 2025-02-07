090354e4e48fe9d032c16230883faf251412f6e8
amond2 <8hyojae@gmail.com>
2025-02-07 20:36:35 +0900

    feat: 아이템 사용 가능하게 함

--- TextRPG/TextRPG/Program_HJ.cs
+++ TextRPG/TextRPG/Program_HJ.cs
@@ -23,7 +23,7 @@ class GameManager
         //Item(string name, string type, int value, string description, bool isUsed = false)
         itemList = new List<Item>
         {
-            new Item(name: "작은 붉은 포션", type: Item.ItemType.HpPotion, value: 10, description: "10 HP 회복됩니다.", isUsed: false),
+            new Item(name: "작은 붉은 포션", type: Item.ItemType.HpPotion, value: 10, description: "10 HP 회복됩니다.", isUsed: true),
             new Item(name: "큰 붉은 포션", type: Item.ItemType.HpPotion, value: 20, description: "20 HP 회복됩니다.", isUsed: false)
         };
     }
@@ -51,7 +51,7 @@ class GameManager
         Console.WriteLine("\n사용할 아이템 번호를 입력하세요 (0을 입력하면 취소)");
         int choice;
         
-        // ConsoleUtility 으로 넣어야 함.
+        // *****ConsoleUtility 으로 넣어야 함.
         // 입력 오류 방지
         while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > itemList.Count)
         {
@@ -68,15 +68,15 @@ class GameManager
         
     }
     
-    // 아이템 사용 매서드 (하는중)
+    // 아이템 사용 매서드 
     public void UseItem(Item item)
     {
         if (item.IsUsed && item.Type == Item.ItemType.HpPotion) // 선택된 item.IsUsed가 True 이고 아이템의 타입이 HpPotion 이면
         {
-            // 플레이어의 HP가 상승 , 회복효과 설명, 아이템이 삭제
-            
+            // *****플레이어의 HP가 상승 , 회복효과 설명, 아이템이 삭제
             Console.WriteLine($"{item.Name} 사용! HP +{item.Description}"); // 회복 효과 설명
             itemList.Remove(item); // 사용 후 아이템 삭제
+            
         }
         else
         {
