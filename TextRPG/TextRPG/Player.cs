48db644c8fcee3969386e49a559170d1367ca321
JinY0321 <sunplant0321@gmail.com>
2025-02-07 09:27:10 +0900

    feat : JY - Player 클래스 생성

---
+++ TextRPG/TextRPG/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Player
+{
+    public int level;
+    public string name;
+    public string jobClass;
+    public float attack;
+    public float depense;
+    public int health;
+    public int gold;
+
+    // 생성자
+    public Player(string name)
+    {
+        Name = name;
+        Level = 1;
+        MaxHealth = 100;
+        Health = MaxHealth;
+        AttackPower = 10;
+        Defense = 5;
+    }
+
+
+}
