f8d4095056684eb70bad3eacbbee5a213380f1f1
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-02-07 15:39:37 +0900

    feat : GameManager 추가

--- TextRPG/TextRPG/Program.cs
+++ TextRPG/TextRPG/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace TextRPG
+﻿using System.Collections.Generic;
+
+namespace TextRPG
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //  내용수정
+            GameManager gm = new GameManager();
+            Screen screen = new Screen();
+
+            screen.MainScreen();
+        }
+    }
+
+    //  게임이 시작될 때 필요한 모든 것들을 생성하는 클래스
+    class GameManager
+    {
+        Player player;
+        List<Monster> monsterList;
+        //List<Item> itemList;
+
+        public GameManager()
+        {
+            player = new Player("Chad", "전사", 1, 10, 5, 100, 1500);
+            monsterList = new List<Monster>
+            {
+                new Monster(2,"미니언",15,10),
+                new Monster(3,"공허충", 10, 15),
+                new Monster(5,"대포미니언",25,15)
+            };
+
+            //itemList = new List<Item>
+            //{
+            //    여러가지 아이템
+            //};
         }
     }
 }
--- TextRPG/TextRPG/Screen.cs
+++ TextRPG/TextRPG/Screen.cs
@@ -18,29 +18,29 @@ namespace TextRPG
             Console.WriteLine("전투 시작");
             Console.WriteLine();
             int input = cu.GetInput(1, 2);
-            switch(input)
+            switch (input)
             {
                 case 1: //상태 보기 함수 호출
                     break;
                 case 2: //전투 시작 함수 호출
                     break;
-            } 
+            }
 
         }
 
         public void StatusScreen()
         {
-            Console.WriteLine("상태 보기");
-            Console.WriteLine("캐릭터의 정보가 표시됩니다.");
-            Console.WriteLine();
-            Console.WriteLine($"Lv. {Level("00")}");
-            Console.WriteLine($"{Name} ( {Job} )";
-            Console.WriteLine($"공격력 : {Atk}");
-            Console.WriteLine($"방어력 : {Def}");
-            Console.WriteLine($"체  력 : {Hp}");
-            Console.WriteLine($"Gold : {Gold}");
-            Console.WriteLine("1");
-            Console.WriteLine("0. 나가기");
+            //Console.WriteLine("상태 보기");
+            //Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+            //Console.WriteLine();
+            //Console.WriteLine($"Lv. {Level("00")}");
+            //Console.WriteLine($"{Name} ( {Job} )";
+            //Console.WriteLine($"공격력 : {Atk}");
+            //Console.WriteLine($"방어력 : {Def}");
+            //Console.WriteLine($"체  력 : {Hp}");
+            //Console.WriteLine($"Gold : {Gold}");
+            //Console.WriteLine("1");
+            //Console.WriteLine("0. 나가기");
 
         }
 
