bf51451bbc5f750a330e8964a9de64748e2a7f11
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-02-07 12:03:42 +0900

    feat : Battle 클래스 작성
    1. 클래스 생성자 구현
    2. 전투 시작 시 몬스터 생성 구현
    3. 플레이어 턴에 플레이어가 몬스터를 공격하는 로직 구현
    4. 적 몬스터의 턴에 몬스터가 플레이러를 공격하는 로직 구현

---
+++ TextRPG/TextRPG/Battle.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TextRPG
+{
+    public class Battle
+    {
+        Player player;
+        List<Monster> monsters;
+        List<Monster> battleMonsters;
+
+        //  Battle 클래스 생성자
+        public Battle(Player player, List<Monster> monsters)
+        {
+            this.player = player;
+            this.monsters = monsters;
+            battleMonsters = new List<Monster>();
+        }
+
+        //  전투 시작
+        public void StartBattle()
+        {
+            //  랜덤 한 몬스터 1~3개 생성
+            Random rand = new Random();
+
+            List<Monster> randomMonsters = new List<Monster>();
+
+            randomMonsters = monsters.OrderBy(x => rand.Next()).ToList();
+
+            int monsterCount = rand.Next(0, 3);
+
+            for (int i = 0; i < monsterCount; i++)
+            {
+                battleMonsters.Add(randomMonsters[i]);
+            }
+        }
+
+        //  플레이어의 공격, 공격할 몬스터의 번호를 입력받으면 로직에 따라 그 몬스터의 hp 감소
+        public void PlayerAttack(int monsterNum)
+        {
+            Random rand = new Random();
+
+            float min = -1 * player.attack / 10.0f;
+            float max = player.attack / 10.0f;
+
+            int randomAttack = (int)Math.Ceiling(rand.NextDouble() * (max - min) + min);
+
+            battleMonsters[monsterNum - 1].Hp -= (int)player.attack + randomAttack;
+
+            if (battleMonsters[monsterNum - 1].Hp <= 0)
+            {
+                battleMonsters[monsterNum - 1].IsDead = true;
+            }
+        }
+
+        //  적 몬스터의 턴, 적 몬스터의 공격력을 활용한 로직에 따라 플레이어의 hp 감소
+        public void EnemyPhase()
+        {
+            for(int i = 0; i < battleMonsters.Count; i++)
+            {
+                if (battleMonsters[i].IsDead == false)
+                {
+                    Random rand = new Random();
+
+                    float min = -1 * battleMonsters[i].Atk / 10.0f;
+                    float max = battleMonsters[i].Atk / 10.0f;
+
+                    int randomAttack = (int)Math.Ceiling(rand.NextDouble() * (max - min) + min);
+
+                    player.health -= battleMonsters[i].Atk + randomAttack;
+                }
+            }
+        }
+    }
+}
