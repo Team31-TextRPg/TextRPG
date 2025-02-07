9d478917fa67444e51e89b13f808e5e68955e207
thn011-create <thn011@naver.com>
2025-02-07 09:53:40 +0900

    fix TH - 변수명_약간_수정

---
+++ TextRPG/TextRPG/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Player
+{
+    public int Level;
+    public string Name;
+    public string JobClass;
+    public float Attack;
+    public float Depense;
+    public int Health;
+    public int Gold;
+
+    // 생성자
+    public Player(string name)
+    {
+        Name = name;
+        Level = 1;
+        MaxHealth = 100;
+        Health = MaxHealth;
+        AttackPower = 10;
+        Defense = 5;
+    }
+
+
+}
