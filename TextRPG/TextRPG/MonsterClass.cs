e0ef59587693575ef9f5ceabab7b84cf390f52f4
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-02-10 16:20:43 +0900

    feat : 클래스 복사를 위한 함수 생성
    Monster 클래스를 깊은 복사 하기 위한 Clone() 함수 구현

--- TextRPG/TextRPG/MonsterClass.cs
+++ TextRPG/TextRPG/MonsterClass.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 namespace TextRPG;
 
-public class Monster
+public class Monster : ICloneable
 {
     //프로퍼티는 {get; set;}은 읽기+쓰기, {get;}은 읽기
-    public int Level { get; }
-    public string Name { get; }
+    public int Level { get; set; }
+    public string Name { get; set; }
     public int Hp { get; set; }
-    public int MaxHp { get; }
-    public int Atk { get; }
+    public int MaxHp { get; set; }
+    public int Atk { get; set; }
     public bool IsDead { get; set; }
 
     public Monster(int level, string name, int maxHp, int atk)
@@ -18,6 +18,20 @@ public class Monster
         Hp = maxHp;
         MaxHp = maxHp;
         Atk = atk;
+    }
+
+    public object Clone()
+    {
+        Monster newMonster = new Monster(0,"",0,0);
+
+        newMonster.Level = this.Level;
+        newMonster.Name = this.Name;
+        newMonster.Hp = this.Hp;
+        newMonster.MaxHp = this.MaxHp;
+        newMonster.Atk = this.Atk;
+        newMonster.IsDead = this.IsDead;
+
+        return newMonster;
     }
     public string MonsterDisplay()
     {
