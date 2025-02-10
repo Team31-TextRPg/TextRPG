f58622adab0af5628461c58d783a7c5def06d2b9
JinY0321 <sunplant0321@gmail.com>
2025-02-10 19:40:21 +0900

    전체 머지 및 확인

---
+++ TextRPG/TextRPG/Item.cs
@@ -0,0 +1,48 @@
+namespace TextRPG;
+
+public class Item
+{
+    public enum ItemType
+    {
+        Weapon,
+        Armor,
+        HpPotion
+    }
+    
+    public string Name {get;}
+    public ItemType Type {get;}
+    public int Value {get;}
+    public string Description {get;}
+    public bool IsUsed {get; set;}
+
+    public Item (string name, ItemType type, int value, string description, bool isUsed = false) //아이템 생성자
+    {
+        Name = name;
+        Type = type;
+        Value = value;
+        Description = description;
+        IsUsed = isUsed;
+    }
+
+    public string ItemDisplay() // 좀 이상함. 장착 관련 부분 차용했는데 
+    {
+        string str = IsUsed ? "[사용됨] " : "";
+        str += $"{Name} | {GetTypeString()} | {Description}";
+        return str; 
+    }
+    
+    public string GetTypeString()
+    {
+        return Type switch
+        {
+            ItemType.Weapon => "무기",
+            ItemType.Armor => "방어구",
+            ItemType.HpPotion => "회복약",
+            _ => "알 수 없음"
+        };
+    }
+    
+}
+
+
+
---
+++ TextRPG/TextRPG/MonsterClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace TextRPG;
+
+public class Monster : ICloneable
+{
+    //프로퍼티는 {get; set;}은 읽기+쓰기, {get;}은 읽기
+    public int Level { get; set; }
+    public string Name { get; set; }
+    public int Hp { get; set; }
+    public int MaxHp { get; set; }
+    public int Atk { get; set; }
+    public bool IsDead { get; set; }
+
+    public Monster(int level, string name, int maxHp, int atk)
+    {
+        Level = level;
+        Name = name;
+        Hp = maxHp;
+        MaxHp = maxHp;
+        Atk = atk;
+    }
+
+    public object Clone()
+    {
+        Monster newMonster = new Monster(0,"",0,0);
+
+        newMonster.Level = this.Level;
+        newMonster.Name = this.Name;
+        newMonster.Hp = this.Hp;
+        newMonster.MaxHp = this.MaxHp;
+        newMonster.Atk = this.Atk;
+        newMonster.IsDead = this.IsDead;
+
+        return newMonster;
+    }
+    public string MonsterDisplay()
+    {
+        string str = $"Lv. {Level} | {Name} | {DeadString()}";
+        return str;
+    }
+
+    public string DeadString()
+    {
+        string str = IsDead ? "Dead" : $"HP {Hp}";
+        return str;
+    }
+}
