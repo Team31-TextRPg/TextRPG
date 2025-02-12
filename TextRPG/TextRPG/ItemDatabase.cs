e9e1fd85697380db26f5cb3887abcfb535dd210c
amond2 <8hyojae@gmail.com>
2025-02-12 10:00:48 +0900

    feat : 붉은 포션 10 -> 30 회복으로 변경

--- TextRPG/TextRPG/ItemDatabase.cs
+++ TextRPG/TextRPG/ItemDatabase.cs
@@ -12,7 +12,7 @@ namespace TextRPG
         {
             items = new Dictionary<string, Item>();
             // 게임에 등장하는 모든 아이템 등록
-            items.Add("HealthPotion", new Item("붉은 포션", Item.ItemType.HpPotion, 10, "사용시 체력을 10 회복합니다."));
+            items.Add("HealthPotion", new Item("붉은 포션", Item.ItemType.HpPotion, 30, "사용시 체력을 30 회복합니다."));
             items.Add("OugerSword", new Item("오우거의 칼", Item.ItemType.Weapon, 150, "작지만 강력한 무기입니다. 착용시 공격력이 150 추가됩니다."));
             items.Add("SlimeArmor", new Item("슬라임 전신 갑옷", Item.ItemType.Armor, 120, "적의 칼날이 미끄러집니다. 착용시 방어력이 120 추가됩니다."));
             // >>필요에 따라 추가 아이템 등록<<
